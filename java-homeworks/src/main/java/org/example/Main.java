7488bd653a93dcf90da77d20f29bd1cb64d0f72e
yarigg405 <south-nort@ya.ru>
2025-03-17 00:07:00 +0300

    кастомная реализация ArrayList

--- java-homeworks/src/main/java/org/example/Main.java
+++ java-homeworks/src/main/java/org/example/Main.java
@@ -1,17 +1,29 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        var myList = new MyArrayList<Integer>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        myList.add(1);
+        myList.add(6);
+        myList.add(8);
+        myList.add(2);
+        myList.add(14);
+        myList.add(10);
+        myList.add(1);
+        myList.add(11);
+        myList.add(12);
+        myList.add(9);
+        myList.add(4);
+        myList.add(15);
+        myList.add(7);
+        myList.add(7);
+
+        System.out.println(myList);
+        System.out.println("\n\n\n");
+
+        myList.sort();
+        System.out.println(myList);
     }
 }
